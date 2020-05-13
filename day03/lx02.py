c427c7a4fd513cc7d62c564aaa742d787002b215
zhoujun164 <zhoujun164@163.com>
2020-05-13 22:38:43 +0800

    修改代码

--- day03/lx02.py
+++ day03/lx02.py
@@ -3,13 +3,12 @@
 在控制台中获取一个月份，
 返回改月的天数
 """
-season = int(input("输入一个月份"))
+season = int(input("输入一个月份:"))
 if season < 1 or season > 12:
     print("您输入的月份有误！")
-
-elif season =(1, 3, 5, 7, 8, 10, 12):
-    print(str(season) + "总共有31天")
-elif season =(4.6.911):
-    print(str(season) + "有30天")
 elif season == 2:
-    print(str(season) + "有28天")
+    print(str(season) + "月" + "有28天")
+elif season == 4 or season == 6 or season == 9 or season == 11:
+    print(str(season) + "月" + "有30天")
+else:
+    print(str(season) + "月" + "有31天")

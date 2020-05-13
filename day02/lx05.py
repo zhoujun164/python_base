a688cc308941686113bd22d57162634371d6adce
zhoujun164 <zhoujun164@163.com>
2020-05-13 16:47:14 +0800

    .

--- day02/lx05.py
+++ day02/lx05.py
@@ -1,5 +1,5 @@
 """
-练习01
+练习
 温度换算器（华氏度，摄氏度，开氏度）
 摄氏度 = （华氏度 - 32） / 1.8
 华氏度 =  摄氏度 × 1.8 + 32
@@ -8,15 +8,15 @@
 --在控制台中获取摄氏度，计算华氏度
 --在控制台中获取摄氏度，计算开氏度
 """
-float_hsd = float(input("输入华氏度："))
-float_ssd = (float_hsd - 32) / 1.8
-print("摄氏度为：" + str(float_ssd))
+fahrenheit = float(input("输入华氏度："))
+centigrade = (fahrenheit - 32) / 1.8
+print("摄氏度为：" + str(centigrade))
 
-float_ssd = float(input("输入摄氏度："))
-float_hsd = float_ssd * 1.8 + 32
-print("华氏度为："+str(float_hsd))
+centigrade = float(input("输入摄氏度："))
+fahrenheit = centigrade * 1.8 + 32
+print("华氏度为："+str(fahrenheit))
 
 
-float_hsd = float(input("输入摄氏度："))
-float_ksd = float_hsd + 273.15
-print("开氏度为："+str(float_ksd))
+fahrenheit = float(input("输入摄氏度："))
+kelvin = fahrenheit + 273.15
+print("开氏度为："+str(kelvin))
--- day02/lx06.py
+++ day02/lx06.py
@@ -5,6 +5,6 @@
 """
 
 float_r = float(input("输入圆形半径:"))
-float_d = 3.14 * (float_r ** 2)
+float_d = 3.14 * (float_r ** 2 )
 float_c = 2 * 3.14 * float_r
 print("圆的面积为：{}，圆的周长为：{}".format(str(float_d), str(float_c)))
